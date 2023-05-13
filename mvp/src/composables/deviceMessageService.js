0f6c3afb1d0c9cb3abac75fa8ee618a8ebdab804
David Yachabach <yachabach@gmail.com>
2023-05-13 16:57:22 -0400

    Updating programRules

--- mvp/src/composables/deviceMessageService.js
+++ mvp/src/composables/deviceMessageService.js
@@ -1,88 +1,61 @@
-import { SerialCodes } from "./serialCodes"
-import { JsonFunctions } from "./jsonFunctions";
-import deviceCodes from '@/data/deviceCodes.json'
-import { parameterCodes } from '@/defaultConfigs/phoenix100.json'
-
-const sc = SerialCodes();
-const { getValueByPath } = JsonFunctions()
+import { parameterCodes, protocol, commands } from '@/defaultConfigs/phoenix100.json'
+import { programRules } from '@/composables/programRules.js'
 
 export const DeviceMessageService = () => {
+
+    const parameterKeys = Object.keys(parameterCodes)
+    const ruleKeys = Object.keys(programRules)
 
     const  convertDurationtoSeconds = duration => {
         const [hours, minutes, seconds] = duration.split(':');
         return Number(hours) * 60 * 60 + Number(minutes) * 60 + Number(seconds);
     }
 
-    const seekProperValue = (key, value) => {
-        if (!deviceCodes[key]) {
-            console.log('Key ', key, ' does not exist in device code json file')
-            return undefined
-        }
-        const ruleValue = deviceCodes[key].rules[String(value).toLowerCase()]
-        if (!ruleValue) {
-            return deviceCodes[key].rules['default']
+    const applyRule = (key, value) => {
+        if (Number(value)) {
+            return value
         } else {
-            return ruleValue
+            return ruleKeys.includes(key) ? programRules[key](value) : programRules.default
         }
     }
 
-    const getValidatedValue = (program, key) => {
-        let value = getValueByPath(deviceCodes[key].dataPath, program)
-        if (!isNaN(1 * value)) value *= 1
-        console.log('value: ', value)
-        if (typeof(value) !== 'number' ) {
-            console.log('Seeking proper value')
-            value = seekProperValue(key, value)
-        }
-        return value
-    }
-
-    const buildMessageCore = (keys, program) => {
-        let core = []
-        keys.forEach(key => {
-            core = core.concat([deviceCodes[key].code, 
-                getValidatedValue(program, key)])
+    const buildMessageCore = program => {
+        return parameterKeys.reduce((msg, key) => {
+            msg.concat(parameterCodes[key], applyRule(key, program[key]))
         })
-        return core
     }
 
     const commandMessageBuilder = (command, payload='') => {
-        let deviceCommand = [sc.com_protocol.msgInit]
+        let deviceCommand = [protocol.msgInit]
         deviceCommand = deviceCommand.concat([(payload.length*2) + 4], command)
         if (payload) {
             deviceCommand = deviceCommand.concat(payload)
         }
         return deviceCommand
     }
-    
+
     const programMessageBuilder = program => {
-        /**Create the message by iterating through the parameter device codes
-         * 
-         */
-        console.log('current program: ', program)
-        let programMessage = []
-        const keys = Object.keys(deviceCodes)
-        const payload = buildMessageCore(keys, program)
-        programMessage = commandMessageBuilder(sc.pc_to_device.write, payload)
-        return programMessage
+        const payload = buildMessageCore(program)
+        programMessage = commandMessageBuilder(commands.write, payload)
+
     }
 
     // const connectDevice = commandMessageBuilder([0x36])
-    const connectDevice = commandMessageBuilder([sc.pc_to_device.init])
+    const connectDevice = commandMessageBuilder([commands.init])
 
-    const ACK = commandMessageBuilder([sc.com_protocol.ACK])
+    const ACK = commandMessageBuilder([protocol.ACK])
 
-    const NACK = commandMessageBuilder([sc.com_protocol.NACK])
+    const NACK = commandMessageBuilder([protocol.NACK])
 
-    const READ = commandMessageBuilder([sc.pc_to_device.read])
+    const READ = commandMessageBuilder([commands.read])
 
-    const WRITE = commandMessageBuilder([sc.pc_to_device.write])
+    const WRITE = commandMessageBuilder([commands.write])
 
-    const RESET = commandMessageBuilder([sc.pc_to_device.reset])
+    const RESET = commandMessageBuilder([commands.reset])
 
-    const PHONE = commandMessageBuilder([sc.pc_to_device.phone])
+    const PHONE = commandMessageBuilder([commands.phone])
 
-    const SETTIME = commandMessageBuilder([sc.pc_to_device.setTime])
+    const SETTIME = commandMessageBuilder([commands.setTime])
 
     return {
         programMessageBuilder,
